8dfaf81e33f9a3035613a84875a8b59adac059fb
mayu <mayutaro89@yahoo.co.jp>
2017-02-01 04:56:15 +0000

    try

---
+++ vendor/bundle/ruby/2.3.0/gems/debug_inspector-0.0.2/ext/debug_inspector/siteconf20170201-1853-rogb0a.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20170201-1853-1g9wv0v"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
